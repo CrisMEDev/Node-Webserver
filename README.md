989ce4b1ada8afc486f830736a6c1feed50e12e1
Cristian Morales <cr15moresp@gmail.com>
2021-05-31 11:48:11 -0500

    Se creó el server con diferentes tipos de respuesta

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Webserver con NODE
 
 Repositorio con código para levantar un webserver
+
+
+
+
+### Notas
+La **request** es lo que se está solicitando por parte del cliente (cualquier cosa que intente acceder a un recurso del servidor).
+
+La **reponse** es lo que el server responde cuando se procese esa request.
+
--- index.js
+++ index.js
@@ -2,7 +2,25 @@ const http = require('http');
 
 http.createServer( ( request, response ) => {
 
-    response.write('Hola mundo');
+    // response.writeHead(200, { 'Content-Type': 'text/plain' });  // Indica que devuelve un objeto de texto plano
+
+    // response.writeHead(200, { 'Content-Type': 'application/json' });  // Indica que devuelve un objeto de json
+    
+    // const persona = {
+    //     id: 1,
+    //     nombre: 'Cristian'
+    // }
+    // response.write( JSON.stringify( persona ) );  // Para que el objeto sea serializable al write, se puede usar JSON
+
+
+    response.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    response.writeHead(200, { 'Content-Type': 'application/csv' });  // Indica que devuelve un objeto de csv
+
+    response.write( 'id, nombre\n' );
+    response.write( '1, Cristian\n' );
+    response.write( '2, Aurora\n' );
+    response.write( '3, Jenifer\n' );
+
     response.end();                     // Para que node sepa que ya se terminó de escribir la respuesta del server
 
 })
